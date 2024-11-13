33976123c0e0240d88d26fb656300e5d28a2f03b
Yeadon8888 <chenmingyi1214@163.com>
2024-11-13 20:59:48 +0800

    版本回滚

--- popup/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('subscribe').addEventListener('click', () => {
-    window.location.href = 'subscribe.html';
-});
-
-document.getElementById('manual-auth').addEventListener('click', () => {
-    window.location.href = 'auth.html';
-}); 
